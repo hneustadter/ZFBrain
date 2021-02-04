3652440805f2fab271a13653290ee8caa7708791
Patrick Eastham <peastham@math.fsu.edu>
2021-02-03 21:39:52 -0500

    testing a change to readthedocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -79,12 +79,15 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_theme
+#import sphinx_theme
+#html_theme = "stanford_theme"
+#html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
+import stanford_theme
 html_theme = "stanford_theme"
-html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
+html_theme_path = [stanford_theme.get_html_theme_path()]
 
 # All available themes:
-print(sphinx_theme.THEME_LIST)
+#print(sphinx_theme.THEME_LIST)
 # >> ['stanford_theme', 'neo_rtd_theme']
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -96,7 +99,7 @@ print(sphinx_theme.THEME_LIST)
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
